fa64e90b3af0ca24483792c1a1cdf8740a291ac9
rawazkader85 <rkade103@uottawa.ca>
2018-11-08 21:30:21 -0500

    This is the first commit to the project. I've worked up to lecture 64 of the course.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] AudioClip breakSound;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    //Configuration parameters 
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;      //Limit to the left in units
+    [SerializeField] float maxX = 15f;    //Limit to the right in units
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Debug.Log(mousePos);
+        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
+        transform.position = paddlePos;
+	}
+}
